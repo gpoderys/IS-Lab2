ef0a3b5f6226709b9e1b7c001c328ec6bcf27460
Gintautas Poderys <gintautasp25@gmail.com>
2021-12-13 21:50:54 +0200

    Strukturos pasirinkimas

--- IS_lab_2.m
+++ IS_lab_2.m
@@ -8,3 +8,20 @@ x2 = [0.1:1/22:1];
 d =  (1 + 0.6*cos(2*pi*x1/0.7)) + 0.3*sin(2*pi*x2)./2;
 figure(1), plot(x1, d), grid on;
 
+% 2. Pasirinkti struktūrą
+% pirmo (paslėptojo) sluoksnio ryšių svoriai
+w11_1 = randn(1);
+w21_1 = randn(1);
+w31_1 = randn(1);
+w41_1 = randn(1);
+b1_1 = randn(1);
+b2_1 = randn(1);
+b3_1 = randn(1);
+b4_1 = randn(1);
+
+% antro sluoksnio(išėjimo) ryšių svoriai
+w11_2 = randn(1);
+w12_2 = randn(1);
+w13_2 = randn(1);
+w14_2 = randn(1);
+b1_2 = randn(1);
